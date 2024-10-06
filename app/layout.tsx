8665183ec5ce2850e9fbb36826b6b89d2f71b2bb
Zachary Stout <stout.zachary@gmail.com>
2024-10-06 10:38:35 -0600

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-center flex flex-col items-center justify-center h-screen`}
       >
         {children}
       </body>
